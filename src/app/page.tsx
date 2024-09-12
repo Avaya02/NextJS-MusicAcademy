5740ad09a0cfb6c216b056f9ec6dc32c16eb419e
Avaya Sharma <avaya.sharma933@gmail.com>
2024-09-12 22:38:17 +0530

    Added navbar comp

---
+++ src/app/page.tsx
@@ -0,0 +1,8 @@
+import Image from "next/image";
+import "./globals.css";
+
+export default function Home() {
+  return (
+   <h1 className="text-2xl text-center mt-4 bg-cyan-300"> Chai aur code  </h1>
+  );
+}
