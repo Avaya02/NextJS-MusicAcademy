36534af9cfc8b0f4d245c37b2b1c11faa183e264
Avaya Sharma <avaya.sharma933@gmail.com>
2024-09-23 21:28:03 +0530

    Styling and props passed

--- src/components/TestimonialCards.tsx
+++ src/components/TestimonialCards.tsx
@@ -1,4 +1,10 @@
 'use client'
+import { InfiniteMovingCards } from "./ui/infinite-moving-cards";
+
+
+
+
+
 const musicSchoolTestimonials = [
     {
       quote:
@@ -34,9 +40,19 @@ const musicSchoolTestimonials = [
 
 function TestimonialCards() {
   return (
-    <div>
-      Testimonial Cards
+    <div className='h-[40rem] w-full dark:bg-black dark:bg-grid-white/[0.2] relative flex flex-col justify-center items-center overflow-hidden '>
+    <h2 className='text-3xl font-bold text-center mb-8 z-10'>Hear Our Harmony : Voices Of Success</h2>
+    <div className='flex justify-center w-full overflow-hidden px-4 sm:px-6 lg:px-8'>
+        <div className='w-full max-w-6xl'>
+        <InfiniteMovingCards
+        items={musicSchoolTestimonials} //the manipulated data which was defined above jsx 
+        direction="right"
+        speed="slow"
+    />
+
+        </div>
     </div>
+</div>
   )
 }
 
