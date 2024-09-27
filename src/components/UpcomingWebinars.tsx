32fb2b22a008fa42483aa93fca67a08a31accb34
Avaya Sharma <avaya.sharma933@gmail.com>
2024-09-27 23:57:08 +0530

    Webinars code

--- src/components/UpcomingWebinars.tsx
+++ src/components/UpcomingWebinars.tsx
@@ -48,8 +48,34 @@ function UpcomingWebinars() {
     },
   ];
   return (
-    <div>
-      Upcoming Webinars
+    <div className="p-12 bg-gray-900">
+      <div className="mx-auto px-4 sm:px-6 max-w-7xl">
+        <div className="text-center">
+          <h2 className="text-base font-semibold tracking-wide bg-gradient-to-r from-teal-100 to-teal-800 text-transparent bg-clip-text uppercase">
+            Featured Webinar
+          </h2>
+          <p className="text-3xl leading-8 font-extrabold tracking-tight text-white sm:text-4xl">
+            Enhance Your Music Journey
+          </p>
+        </div>
+        <div className="mt-10">
+          <HoverEffect
+            items={featuredWebinars.map((webinar) => ({
+              title: webinar.title,
+              description: webinar.description,
+              link: webinar.slug,
+            }))}
+          />
+        </div>
+        <div className="mt-10 text-center ">
+          <Link
+            href={"/courses"}
+            className="px-4 py-2 rounded border border-gray-500 text-neutral-700 bg-white hover:bg-gray-300 transition duration-200"
+          >
+            View All Webinar
+          </Link>
+        </div>
+      </div>
     </div>
   )
 }
