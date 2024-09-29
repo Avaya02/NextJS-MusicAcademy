6bde9af7a503b5ba03050ab53b0588b7133155ad
Avaya Sharma <avaya.sharma933@gmail.com>
2024-09-30 00:51:09 +0530

    Fixed images errors

--- src/components/Instructors.tsx
+++ src/components/Instructors.tsx
@@ -40,6 +40,7 @@ function Instructors() {
         <h2 className="text-2xl md:text-4xl lg:text-7xl text-white font-bold text-center mb-8">Meet Our Instructors</h2>
         <p className="text-base md:text-lg text-white text-center mb-4">Discover the talented professionals who will guide your musical journey</p>
         <div className='flex justify-center items-center w-full mb-10'>
+            
             <AnimatedTooltip items={instructors}/>
 
         </div>
